b9721c5a35a266cb155568b53ccc887fbeebf1d9
x1kmatov272 <kamronbekxikmatov50@gmail.com>
2025-03-13 10:05:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let string = prompt('string typedagi malumot kiriting')
+console.log(string)
+
+
+let number = prompt('number typedagi malumot kiriting')
+console.log(number)
+
+
+let Boolean = prompt('boolean typedagi malumot kiriting')
+console.log(Boolean)
+
+
+alert(`Hamma malumotlarni togri kiritganingizga ishoncingiz komilmi?`)
+
+alert(`Siz kiritgan Kamron sozi stringga tegishli  \nSiz kiritgan 13 sozi numberga tegishli \nSiz kiritgan true sozi booleanga tegishli`)
